9cf2a36752638b1be9cd3ce0a3a2b5d9fb3a239d
tripledrive <jtk93.x29@gmail.com>
2024-08-16 01:29:04 +0900

    過去の王道

---
+++ oudou03/index.js
@@ -0,0 +1,15 @@
+function submit_answer(){
+    if(document.getElementById("answer").value == "クリスタブララーサ"){
+        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
+    }else if(document.getElementById("answer").value == "クリス=タブラ=ラーサ"){
+        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
+    }else if(document.getElementById("answer").value == "クリス＝タブラ＝ラーサ"){
+        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+document.getElementById("ch_button").onclick = function() {
+    window.location.href = "./s0sd2.html"
+}
