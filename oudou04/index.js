6f45aeb374d56c05cbe3938dd67892df3388715d
tripledrive <jtk93.x29@gmail.com>
2024-08-16 01:56:31 +0900

    窮鼠の王道

--- oudou04/index.js
+++ oudou04/index.js
@@ -1,27 +1,29 @@
 function submit_answer_s1(){
-    if(document.getElementById("answer").value == "クリスタブララーサ"){
-        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
-    }else if(document.getElementById("answer").value == "クリス=タブラ=ラーサ"){
-        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
-    }else if(document.getElementById("answer").value == "クリス＝タブラ＝ラーサ"){
-        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
+    if(document.getElementById("answer").value == "キュウキョクシンカ"){
+        window.location.href = "./sqw2v.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_answer_s2(){
+    if(document.getElementById("answer").value == "キュウキョクギンガ"){
+        window.location.href = "./ytp3v.html"
     }else{
         document.getElementById("msg").classList.remove('none');
     }
 }
 
 function submit_answer_s3(){
-    if(document.getElementById("answer").value == "クリスタブララーサ"){
-        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
-    }else if(document.getElementById("answer").value == "クリス=タブラ=ラーサ"){
-        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
-    }else if(document.getElementById("answer").value == "クリス＝タブラ＝ラーサ"){
-        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション2「推測の王道」"
+    if(document.getElementById("answer").value == "Z"){
+        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション4「窮鼠の王道」"
+    }else if(document.getElementById("answer").value == "Ｚ"){
+        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション4「窮鼠の王道」"
     }else{
         document.getElementById("msg").classList.remove('none');
     }
 }
 
 document.getElementById("ch_button").onclick = function() {
-    window.location.href = "./s0sd2.html"
+    window.location.href = "./qk0p1.html"
 }
