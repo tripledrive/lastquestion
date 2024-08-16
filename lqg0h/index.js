ef8fdb18060764f8279017b94620e5f945ddaf87
tripledrive <jtk93.x29@gmail.com>
2024-08-17 00:07:14 +0900

    lastq

---
+++ lqg0h/index.js
@@ -0,0 +1,25 @@
+function submit_answer(){
+    if(document.getElementById("answer").value == "OOPARTS"){
+        window.location.href = "https://tripledrive.github.io/lastquestion/lqg0h/jgv9rswtg.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_tn(){
+    gen_QR(document.getElementById('tb').value);
+}
+
+function gen_QR(team_no) {  
+    // リクエストクエリを取得してQRコードを生成する
+    var inp01 = "https://island-juniper-narwhal.glitch.me/?contents=&team_no="+team_no+"&mission=lastquestion"
+    document.getElementById('qr').textContent = '';
+      // QRコード作成
+    var qrcode_1 = new QRCode('qr', {
+        text:  inp01,
+        width: 128,
+        height: 128,
+        correctLevel : QRCode.CorrectLevel.H 
+    })
+    document.getElementById("hint").classList.remove('none')
+}
