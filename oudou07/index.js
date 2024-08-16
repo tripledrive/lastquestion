d749531f8f6a8d48eb1a37cfad9cb552d6fd13d4
tripledrive <jtk93.x29@gmail.com>
2024-08-17 02:45:14 +0900

    黒箱

---
+++ oudou07/index.js
@@ -0,0 +1,51 @@
+function submit_answer_1(){
+    if(document.getElementById("answer").value == "アバク"){
+        window.location.href = "./2rghq.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_answer_2(){
+    if(document.getElementById("answer").value == "ミロク"){
+        window.location.href = "./gh3bd.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_answer_3(){
+    if(document.getElementById("answer").value == "メーテル"){
+        window.location.href = "./ajt4v.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_answer_4(){
+    if(document.getElementById("answer").value == "ジャスミン"){
+        window.location.href = "./jyx5q.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_answer_5(){
+    if(document.getElementById("answer").value == "イゴゴーゴー"){
+        window.location.href = "./6gg3e.html"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+function submit_answer_6(){
+    if(document.getElementById("answer").value == "クローンバイス"){
+        window.location.href = "https://island-juniper-narwhal.glitch.me/?contents=ミッション7「黒箱の王道」"
+    }else{
+        document.getElementById("msg").classList.remove('none');
+    }
+}
+
+document.getElementById("ch_button").onclick = function() {
+    window.location.href = "./qk0p1.html"
+}
